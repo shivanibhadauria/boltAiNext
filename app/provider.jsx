7cb238c8f5e35915da49d71fe6853751b8ec8ad4
Shivani Bhadauria <shivanibhadauria921997@gmal.com>
2025-01-24 10:03:17 +0530

    google auth added

--- app/provider.jsx
+++ app/provider.jsx
@@ -4,25 +4,30 @@ import Header from "@/components/custom/Header";
 import { MassageContext } from "@/context/MassageContext";
 import { useState } from "react";
 import { UserContext } from "@/context/UserContext";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const Provider = ({ children }) => {
   const [massage, setMassage] = useState();
   const [userDetail, setUserDetail] = useState();
 
   return (
-    <UserContext.Provider value={{ userDetail, setUserDetail }}>
-      <MassageContext.Provider value={{ massage, setMassage }}>
-        <NextThemesProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Header />
-          <div>{children}</div>
-        </NextThemesProvider>
-      </MassageContext.Provider>
-    </UserContext.Provider>
+    <GoogleOAuthProvider
+      clientId={process.env.NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_ID_KEY}
+    >
+      <UserContext.Provider value={{ userDetail, setUserDetail }}>
+        <MassageContext.Provider value={{ massage, setMassage }}>
+          <NextThemesProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <div>{children}</div>
+          </NextThemesProvider>
+        </MassageContext.Provider>
+      </UserContext.Provider>
+    </GoogleOAuthProvider>
   );
 };
 
--- components/custom/SignIn.jsx
+++ components/custom/SignIn.jsx
@@ -1,13 +1,28 @@
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog";
+import { UserContext } from "@/context/UserContext";
+
+import { useGoogleLogin } from "@react-oauth/google";
+import { useContext } from "react";
+import axios from "axios";
 
 const SignIn = ({ openDialog, closeDialog }) => {
+  const { userDetail, setUserDetail } = useContext(UserContext);
+
+  const googleLogin = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      console.log(tokenResponse);
+      const userInfo = await axios.get(
+        "https://www.googleapis.com/oauth2/v3/userinfo",
+        { headers: { Authorization: "Bearer" + tokenResponse.access_token } }
+      );
+
+      console.log(userInfo);
+      setUserDetail(userInfo?.data);
+      closeDialog(false);
+    },
+    onError: (errorResponse) => console.log(errorResponse),
+  });
+
   return (
     <Dialog open={openDialog} onOpenChange={closeDialog}>
       <DialogContent>
@@ -17,7 +32,10 @@ const SignIn = ({ openDialog, closeDialog }) => {
             <p className=" text-white/60 font-sm text-wrap ">
               To use Bolt you must log into an existing account or create one.
             </p>
-            <button className=" bg-blue-500 rounded-lg p-2 cursor-pointer ">
+            <button
+              onClick={googleLogin}
+              className=" bg-blue-500 rounded-lg p-2 cursor-pointer "
+            >
               Sign in with google
             </button>
             <p className=" text-xs text-white/60  ">
